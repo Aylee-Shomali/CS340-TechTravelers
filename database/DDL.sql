3033eed9cb230b50d333de3572f7d0406f7316d7
Aylee Shomali <shomalay@flip2.engr.oregonstate.edu>
2023-05-21 20:32:24 -0700

    Added SELECT functionality for customerReservations, stylesheet, and index page.

---
+++ database/DDL.sql
@@ -0,0 +1,194 @@
+-- Group 6 TechTravelers Project Step 3 Final
+-- Group Members: Sajidah Wahdy and Aylee Shomali
+-- DDL + Insert statements for TechTravels Database.
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET AUTOCOMMIT = 0;
+
+-- -----------------------------------------------------
+-- Table `Locations`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Locations` ;
+
+CREATE TABLE IF NOT EXISTS `Locations` (
+  `locationId` INT(50) NOT NULL AUTO_INCREMENT,
+  `cityName` VARCHAR(50) NOT NULL,
+  `stateOrProvince` VARCHAR(50),
+  `countryName` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`locationId`),
+  UNIQUE KEY location (cityName, countryName));
+  
+-- -----------------------------------------------------
+-- Table `Customers`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Customers` ;
+
+CREATE TABLE IF NOT EXISTS `Customers` (
+  `customerId` INT(50) NOT NULL AUTO_INCREMENT,
+  `firstName` VARCHAR(50) NOT NULL,
+  `lastName` VARCHAR(80) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `phoneNumber` VARCHAR(15) NOT NULL,
+  `address` VARCHAR(700) NOT NULL,
+  PRIMARY KEY (`customerId`),
+  UNIQUE KEY full_name (firstName, lastName));
+
+-- -----------------------------------------------------
+-- Table `Agents`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Agents` ;
+
+CREATE TABLE IF NOT EXISTS `Agents` (
+  `agentId` INT(50) NOT NULL AUTO_INCREMENT,
+  `firstName` VARCHAR(50) NOT NULL,
+  `lastName` VARCHAR(80) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `phoneNumber` VARCHAR(15) NOT NULL,
+  `locationId` INT(50),
+  PRIMARY KEY (`agentId`),
+  UNIQUE KEY full_name (firstName, lastName),
+  CONSTRAINT `locationId`
+    FOREIGN KEY (`locationId`)
+    REFERENCES `Locations` (`locationId`)
+    ON DELETE NO ACTION
+    ON UPDATE CASCADE);
+
+-- -----------------------------------------------------
+-- Table `Reservations`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Reservations` ;
+
+CREATE TABLE IF NOT EXISTS `Reservations` (
+  `reservationId` INT(50) NOT NULL AUTO_INCREMENT,
+  `agentId` INT(50) NOT NULL,
+  `startDate` DATE NULL,
+  `endDate` DATE NULL,
+  PRIMARY KEY (`reservationId`),
+  CONSTRAINT `agentId`
+    FOREIGN KEY (`agentId`)
+    REFERENCES `Agents` (`agentId`)
+    ON DELETE NO ACTION
+    ON UPDATE CASCADE);
+
+
+-- -----------------------------------------------------
+-- Table `CustomerReservation`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `CustomerReservation` ;
+
+CREATE TABLE IF NOT EXISTS `CustomerReservation` (
+  `customerReservationId` INT(50) NOT NULL AUTO_INCREMENT,
+  `customerId` INT(50),
+  `reservationId` INT(50),
+  PRIMARY KEY (`customerReservationId`),
+  CONSTRAINT `customerId`
+    FOREIGN KEY (`customerId`)
+    REFERENCES `Customers` (`customerId`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION,
+  CONSTRAINT `reservationId`
+    FOREIGN KEY (`reservationId`)
+    REFERENCES `Reservations` (`reservationId`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table `ReservationLocation`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `ReservationLocation` ;
+
+CREATE TABLE IF NOT EXISTS `ReservationLocation` (
+  `reservationLocationId` INT(50) NOT NULL AUTO_INCREMENT,
+  `locationId` INT(50) NOT NULL,
+  `reservationId` INT(50) NOT NULL,
+  PRIMARY KEY (`reservationLocationId`),
+  FOREIGN KEY (`locationId`)
+  REFERENCES `Locations` (`locationId`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION,
+  FOREIGN KEY (`reservationId`)
+  REFERENCES `Reservations` (`reservationId`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION);
+
+-- Insert statements. --
+
+-- Add location data.
+INSERT INTO Locations(cityName, stateOrProvince, countryName) VALUES
+('Cairo', NULL, 'Egypt'),
+('Istanbul', NULL, 'Turkey'),
+('Rome', 'Lazio', 'Italy'),
+('Milan', 'Lombardia', 'Italy'),
+('London', NULL, 'England');
+
+-- Add travel agents data.
+INSERT INTO Agents (firstName, lastName, email, phoneNumber, locationId) VALUES
+('Bob', 'Nelson', 'bnelson@techtravelers.com', '503-671-9922', 
+  (SELECT locationId 
+   FROM Locations 
+   WHERE cityName = 'Cairo' AND countryName = 'Egypt')
+),
+('Janice', 'Mohammed', 'jmohammed@techtravelers.com', '971-275-5719', NULL),
+('Alex', 'White', 'awhite@techtravelers.com', '877-371-9992', 
+  (SELECT locationId 
+   FROM Locations 
+   WHERE cityName = 'London' AND countryName = 'England')
+ );
+
+-- Add customer data.
+INSERT INTO Customers(firstName, lastName, email, phoneNumber, address) VALUES
+('Larry', 'Ellison', 'lellison@oracle.com', '371-473-3838', '928 Lombard Street, San Francisco, CA 94133'),
+('Samara', 'Sudarshana', 'samara.sudarshana@me.com', '847-284-2283', '232, 228, Nurshina Bldg, L.t.marg, Chira Bazar, Mumbai, Maharashtra 400002 India'),
+('Badri', 'Sudarshana', 'badri.sudarshana@gmail.com', '473-284-4473', '3526 Kelly Parkway, Belize District, Belize'),
+('Giovanni', 'Castelucci', 'giovanni.castelucci@yahoo.com', '579-482-3383', '27592 Nicholson Ct., Miami, Florida 33101');
+
+-- Add reservation data.
+INSERT INTO Reservations (agentId, startDate, endDate) VALUES 
+((SELECT agentId 
+  FROM Agents 
+  WHERE firstName = 'Bob' AND lastName = 'Nelson'
+ ),
+ '2023-10-03', '2023-10-12'),
+((SELECT agentId 
+  FROM Agents 
+  WHERE firstName = 'Alex' AND lastName = 'White'
+ ),
+ '2023-09-05', '2023-09-19'),
+((SELECT agentId 
+  FROM Agents 
+  WHERE firstName = 'Bob' AND lastName = 'Nelson'
+ ),
+ '2023-04-11', '2023-04-29');
+
+-- Fill reservation/location intersection table.
+INSERT INTO ReservationLocation(locationId, reservationId) VALUES
+((SELECT locationId 
+   FROM Locations 
+   WHERE cityName = 'Cairo' AND countryName = 'Egypt'), 1),
+((SELECT locationId 
+   FROM Locations 
+   WHERE cityName = 'Istanbul' AND countryName = 'Turkey'), 1),
+((SELECT locationId 
+   FROM Locations 
+   WHERE cityName = 'Rome' AND countryName = 'Italy'), 2),
+((SELECT locationId 
+   FROM Locations 
+   WHERE cityName = 'London' AND countryName = 'England'), 3);
+
+-- Fill customer/reservation intersection table.
+INSERT INTO CustomerReservation(customerId, reservationId) VALUES
+((SELECT customerId 
+   FROM Customers
+   WHERE firstName = 'Samara' AND lastName = 'Sudarshana'), 1),
+((SELECT customerId 
+   FROM Customers
+   WHERE firstName = 'Badri' AND lastName = 'Sudarshana'), 1),
+((SELECT customerId 
+   FROM Customers
+   WHERE firstName = 'Samara' AND lastName = 'Sudarshana'), 3);
+
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+COMMIT;
---
+++ database/DML.sql
@@ -0,0 +1,225 @@
+-- Group 6 TechTravelers Project Step 3 Final
+-- Group Members: Sajidah Wahdy and Aylee Shomali
+-- DML file for TechTravels Database HTML pages.
+
+-- ':' indicates a variable derived from user with JavaScript in a later step.
+
+-- -----------------------------------------------------
+-- Select Query for Agents page for Read operation.
+-- -----------------------------------------------------
+SELECT agentId AS `Agent Id`, 
+       firstName AS `First Name`, 
+       lastName AS `Last Name`, 
+       email AS `Email`, 
+       phoneNumber AS `Phone Number`, 
+       CONCAT(cityName,', ', IF(stateOrProvince IS NOT NULL, CONCAT(stateOrProvince, ', '), ''), countryName) AS `Location`
+FROM Agents
+INNER JOIN Locations 
+ON Agents.locationId = Locations.locationId
+ORDER BY agentId ASC;
+
+-- -----------------------------------------------------
+-- Select Query for Agents page to show Location dropdown options.
+-- -----------------------------------------------------
+SELECT locationId, CONCAT(cityName,', ', IF(stateOrProvince IS NOT NULL, CONCAT(stateOrProvince, ', '), ''), countryName) AS `Location`
+FROM Locations 
+ORDER BY locationId ASC;
+
+-- -----------------------------------------------------
+-- Insert query for Agents page for Create operation using user input from HTML form.
+-- -----------------------------------------------------
+INSERT INTO agents (firstName, lastName, email, phoneNumber, locationId) 
+VALUES (:firstName, :lastName, :email, :phoneNumber, :locationIdFromDropdown);
+
+-- -----------------------------------------------------
+-- Select query for Locations page Read operation.
+-- -----------------------------------------------------
+SELECT locationId AS `Location Id`, 
+       cityName AS `City Name`, 
+       stateOrProvince AS `State/Province`, 
+       countryName AS `Country Name`
+FROM Locations
+ORDER BY locationId ASC;
+
+-- -----------------------------------------------------
+-- Insert query for Locations page Create operation using user input from HTML form.
+-- -----------------------------------------------------
+INSERT INTO Locations (cityName, stateOrProvince, countryName)
+VALUES (:cityName, :stateOrProvince, :countryName);
+
+-- -----------------------------------------------------
+-- Select query for CustomerReservation page Read operation.
+-- -----------------------------------------------------
+SELECT customerReservationId AS `Customer Reservation Id`, 
+       CONCAT(Customers.firstName, ' ', Customers.lastName) AS `Customer`, reservationId AS `Reservation Id`
+FROM CustomerReservation
+JOIN Customers ON CustomerReservation.customerId = Customers.customerId
+ORDER BY customerReservationId ASC;
+
+-- -----------------------------------------------------
+-- Select query for Reservation page Read operation (Also used in CustomerReservations page for reference).
+-- -----------------------------------------------------
+SELECT reservationId AS `Reservation Id`, 
+       CONCAT(Agents.firstName, ' ', Agents.lastName) AS `Agent`, 
+       startDate AS `Start Date`, 
+       endDate AS `End Date`
+FROM Reservations
+JOIN Agents ON Reservations.agentId = Agents.agentId
+ORDER BY reservationId ASC;
+
+-- -----------------------------------------------------
+-- Select Query for CustomerReservation page to show Customer dropdown options.
+-- -----------------------------------------------------
+SELECT customerId, CONCAT(Customers.firstName, ' ', Customers.lastName) AS `Customer`
+FROM Customers 
+ORDER BY customerId ASC;
+
+-- -----------------------------------------------------
+-- Select Query for CustomerReservation page to show Reservation dropdown options.
+-- -----------------------------------------------------
+SELECT reservationId
+FROM Reservations 
+ORDER BY reservationId ASC;
+
+-- -----------------------------------------------------
+-- Insert query for CustomerReservation page Create operation using user input from HTML form.
+-- -----------------------------------------------------
+INSERT INTO CustomerReservation (customerId, reservationId)
+VALUES (:customerIdFromDropdown, :reservationIdFromDropdown);
+
+-- -----------------------------------------------------
+-- Select query to show data for existing record for CustomerReservation page Update operation. (Also used to show values in DELETE confirmation message)
+-- -----------------------------------------------------
+SELECT customerReservationId, CONCAT(Customers.firstName, ' ', Customers.lastName) AS Customer, reservationId
+FROM CustomerReservation
+JOIN Customers ON CustomerReservation.customerId = Customers.customerId
+WHERE CustomerReservationId = :id
+ORDER BY customerReservationId ASC;
+
+-- -----------------------------------------------------
+-- Update query for CustomerReservation page Update operation using user input from HTML form.
+-- -----------------------------------------------------
+UPDATE CustomerReservation SET customerId = :customerIdFromDropdown, reservationId= :reservationIdFromDropdown
+WHERE CustomerReservationId = :id
+
+-- -----------------------------------------------------
+-- Dis-associate a Reservation from a Customer (M-to-M relationship deletion)
+-- -----------------------------------------------------
+DELETE FROM CustomerReservation WHERE customerReservationId = :id
+
+
+-- -----------------------------------------------------
+-- Select Query for Customers page for Read operation.
+-- -----------------------------------------------------
+SELECT customerId AS `Customer Id`, 
+       firstName AS `First Name`, 
+       lastName AS `Last Name`, 
+       email AS `Email`, 
+       phoneNumber AS `Phone Number`, 
+       address AS `Address`
+FROM Customers
+ORDER BY customerId ASC;
+
+-- -----------------------------------------------------
+-- Insert query for Customers page for Create operation using user input from HTML form.
+-- -----------------------------------------------------
+INSERT INTO customers (firstName, lastName, email, phoneNumber, address) 
+VALUES (:firstName, :lastName, :email, :phoneNumber, :address);
+
+-- -----------------------------------------------------
+-- Select query to show data for existing record for Customer page Update operation. (Also used to show values in DELETE confirmation message)
+-- -----------------------------------------------------
+SELECT customerId AS `Customer Id`, Customers.firstName AS `First Name`, Customers.lastName AS `Last Name`
+FROM Customers
+WHERE CustomerId = :id
+ORDER BY customerId ASC;
+
+-- -----------------------------------------------------
+-- Update query for Customer page Update operation using user input from HTML form.
+-- -----------------------------------------------------
+UPDATE Customer SET firstName = :firstName, lastName = :lastName, email = :email, phoneNumber = :phoneNumber, address = :address
+WHERE CustomerId = :id
+
+-- -----------------------------------------------------
+-- Delete a Customer.
+-- -----------------------------------------------------
+DELETE FROM Customer WHERE customerId = :id
+
+
+-- -----------------------------------------------------
+-- Select query for Reservations page Read operation.
+-- -----------------------------------------------------
+SELECT reservationId AS `Reservation Id`, 
+       CONCAT(Agents.firstName, ' ', Agents.lastName) AS `Agent`,
+       startDate AS `Start Date`, 
+       endDate AS `End Date`
+FROM Reservations
+JOIN Agents ON Reservations.agentId = Agents.agentId
+ORDER BY reservationId ASC;
+
+-- -----------------------------------------------------
+-- Insert query for Reservations page Create operation using user input from HTML form.
+-- -----------------------------------------------------
+INSERT INTO Reservations (agentId, startDate, endDate)
+VALUES (:agentId, :startDate, :endDate);
+
+-- -----------------------------------------------------
+-- Select query for ReservationLocations page Read operation.
+-- -----------------------------------------------------
+SELECT reservationLocationId AS `Reservation Location Id`, CONCAT(cityName,', ', IF(stateOrProvince IS NOT NULL, CONCAT(stateOrProvince, ', '), ''), countryName) AS `Location`, ReservationLocation.reservationId AS `Reservation Id`
+FROM ReservationLocation
+INNER JOIN Locations ON ReservationLocation.locationId = Locations.locationId
+INNER JOIN Reservations ON Reservations.reservationId = ReservationLocation.reservationId
+ORDER BY reservationLocationId ASC;
+
+-- -----------------------------------------------------
+-- Select query for Reservation page Read operation (Also used in ReservationLocations page for reference).
+-- -----------------------------------------------------
+SELECT reservationId AS `Reservation Id`, 
+       CONCAT(Agents.firstName, ' ', Agents.lastName) AS `Agent`, 
+       startDate AS `Start Date`, 
+       endDate AS `End Date`
+FROM Reservations
+JOIN Agents ON Reservations.agentId = Agents.agentId
+ORDER BY reservationId ASC;
+
+-- -----------------------------------------------------
+-- Select Query for ReservationLocation page to show Location dropdown options.
+-- -----------------------------------------------------
+SELECT locationId, CONCAT(cityName,', ', IF(stateOrProvince IS NOT NULL, CONCAT(stateOrProvince, ', '), ''), countryName) AS `Location`
+FROM Locations 
+ORDER BY locationId ASC;
+
+-- -----------------------------------------------------
+-- Select Query for ReservationLocation page to show Reservation dropdown options.
+-- -----------------------------------------------------
+SELECT reservationId
+FROM Reservations 
+ORDER BY reservationId ASC;
+
+-- -----------------------------------------------------
+-- Insert query for ReservationLocation page Create operation using user input from HTML form.
+-- -----------------------------------------------------
+INSERT INTO ReservationLocation (locationId, reservationId)
+VALUES (:locationIdFromDropdown, :reservationIdFromDropdown);
+
+-- -----------------------------------------------------
+-- Select query to show data for existing record for ReservationLocation page Update operation. (Also used to show values in DELETE confirmation message)
+-- -----------------------------------------------------
+SELECT ReservationLocationId, CONCAT(cityName,', ', IF(stateOrProvince IS NOT NULL, CONCAT(stateOrProvince, ', '), ''), countryName) AS `Location`, Reservations.reservationId AS reservationId
+FROM ReservationLocation
+LEFT JOIN Reservations ON Reservations.reservationId = ReservationLocation.reservationId
+LEFT JOIN Locations ON Locations.locationId = ReservationLocation.locationId
+WHERE reservationLocationId = :id
+ORDER BY reservationLocationId ASC;
+
+-- -----------------------------------------------------
+-- Update query for ReservationLocation page Update operation using user input from HTML form.
+-- -----------------------------------------------------
+UPDATE ReservationLocation SET locationId = :locationIdFromDropdown, reservationId= :reservationIdFromDropdown
+WHERE reservationLocationId = :id
+
+-- -----------------------------------------------------
+-- Dis-associate a Reservation from a Location by deleting from ReservationLocation (M-to-M relationship deletion)
+-- -----------------------------------------------------
+DELETE FROM ReservationLocation WHERE reservationLocationId = :id
