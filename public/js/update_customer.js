d994925f472999b8f8070cb73dda6e8dd380263c
sajidahw <88634981+sajidahw@users.noreply.github.com>
2023-05-30 22:34:40 -0700

    update customer with commentary

--- public/js/update_customer.js
+++ public/js/update_customer.js
@@ -14,7 +14,7 @@ updateCustomerForm.addEventListener("submit", function (e) {
     // Prevent the form from submitting
     e.preventDefault();
 
-    // Get form fields we need to get data from
+    // Get form input original fields we need to get data from and don't rename as inputCustomerIdUpdate but assign it to getElementById with "input-customerId-update"
     let inputCustomerId = document.getElementById("input-customerId-update");
     let inputFirstName = document.getElementById("input-firstName-update");
     let inputLastName = document.getElementById("input-lastName-update");
@@ -22,23 +22,23 @@ updateCustomerForm.addEventListener("submit", function (e) {
     let inputPhoneNumber = document.getElementById("input-phoneNumber-update");
     let inputAddress = document.getElementById("input-address-update");
 
-    // Get the values from the form fields
-    let customerIdValue = inputCustomerId.value;
-    let firstNameValue = inputFirstName.value;
-    let lastNameValue = inputLastName.value;
-    let emailValue = inputEmail.value;
-    let phoneNumberValue = inputPhoneNumber.value;
-    let addressValue = inputAddress.value;
+    // Get the input original values (camelCase) from the form fields above
+    let  = inputCustomerId.value;
+    let  = inputFirstName.value;
+    let  = inputLastName.value;
+    let  = inputEmail.value;
+    let  = inputPhoneNumber.value;
+    let  = inputAddress.value;
 
     // Currently the database table for customer does not allow updating values to NULL
     // So we must abort if being passed NULL for any values.
-
+    // Enforcing that inputs are not empty
     if (isNaN(customerIdValue) 
-        | firstNameValue == ""
-        | lastNameValue == ""
-        | emailValue == "" 
-        | phoneNumberValue == ""
-        | addressValue == "") {
+        |  == ""
+        |  == ""
+        |  == "" 
+        |  == ""
+        |  == "") {
         // Error message here.
         console.log("Null values are not accepted with this input.")
         return;
@@ -46,12 +46,12 @@ updateCustomerForm.addEventListener("submit", function (e) {
 
     // Put our data we want to send in a javascript object
     let data = {
-        customerId: customerIdValue,
-        firstName: firstNameValue,
-        lastName: lastNameValue,
-        email: emailValue,
-        phoneNumber: phoneNumberValue,
-        address: addressValue
+        customerId: ,
+        firstName: ,
+        lastName: ,
+        email: ,
+        phoneNumber: ,
+        address: 
     }
 
     // Setup our AJAX request
@@ -63,8 +63,8 @@ updateCustomerForm.addEventListener("submit", function (e) {
     xhttp.onreadystatechange = () => {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
 
-            // Add the new data to the table
-            updateRow(xhttp.response, customerIdValue);
+            // Add the new data to the table via PK customerIdValue; don't add the other values
+            updateRow(xhttp.response,);
 
         }
         else if (xhttp.readyState == 4 && xhttp.status != 200) {
@@ -92,6 +92,7 @@ function updateRow(data, customerId) {
             let updateRowIndex = table.getElementsByTagName("tr")[i];
 
             // Get td for designated elements of updated values.
+            // [0] is customerId
             let td1 = updateRowIndex.getElementsByTagName("td")[1];//firstName
             let td2 = updateRowIndex.getElementsByTagName("td")[2];//lastName
             let td3 = updateRowIndex.getElementsByTagName("td")[3];//email
@@ -99,6 +100,7 @@ function updateRow(data, customerId) {
             let td5 = updateRowIndex.getElementsByTagName("td")[5];//address
 
             // Reassign field inputs to the values we updated to
+            // [0] remains the same while adding .parameters for updates
             td1.innerHTML = parsedData[0].firstName;
             td2.innerHTML = parsedData[0].lastName;
             td3.innerHTML = parsedData[0].email;
