cbbb5acefc6d0b7911ffb8613ed2bbe0d018f59e
sajidahw <88634981+sajidahw@users.noreply.github.com>
2023-06-09 20:16:17 -0700

    updated port to 8008 as was previously

--- app.js
+++ app.js
@@ -12,7 +12,7 @@
 */
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 8007;                 // Set a port number at the top so it's easy to change in the future
+PORT        = 8008;                 // Set a port number at the top so it's easy to change in the future
 
 // app.js
 const { engine } = require('express-handlebars');
